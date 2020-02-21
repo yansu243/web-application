4bf0740e8fa9285d2408fbf74cb910c2618295b2
Yan Su <yansu243@gmail.com>
2020-02-21 21:52:31 +0800

    set up app for heroku

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoieWFuc3UyNDMiLCJhIjoiY2s2a20wOW90MDVvbjNkdG41c2lzazUyaiJ9.nDnQkepQm37lJonRRT8xEg&limit=1'
+    
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
